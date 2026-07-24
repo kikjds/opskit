a853e2777715df6eeb4daecd270d7379558e2e2e
macolepe <kick.jds@gmail.com>
2026-07-24 14:01:30 +0200

    Added logic for reading yaml files

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import paramiko
 import typer
+import yaml
 from pathlib import PurePosixPath, PureWindowsPath
 
 def connectViaSSH(username: str, ip: str, key: str, password: str = None, port: int = 22):
@@ -17,3 +18,16 @@ def connectViaSSH(username: str, ip: str, key: str, password: str = None, port: 
         print(f"Connecting to {ip} as {username} on port {port} using key: {key_path}")
         client.connect(hostname=ip, port=port, username=username, key_filename=key_path)
     return client
+
+def read_yaml_file(path: str):
+    with open(path, "r") as file:
+        data = yaml.safe_load(file)
+        {'servers': [{'name': 'Server 1', 'HostName': 'larry219.mikrus.xyz', 'User': 'root', 'Port': 10219, 'IdentityFile': 'C:\\Users\\kickj\\.ssh\\id_ed25519'}, {'name': 'Server 2', 'HostName': '83.168.95.12', 'User': 'root', 'Port': 1410, 'Password': 'Siemaneseloop2'}]}
+        names = [server.get("name") for server in data.get("servers", [])]
+        typer.echo(f"Which server do you want to connect to? {names}")
+        selected_server = typer.prompt("Enter the server name")
+        server = next((s for s in data.get("servers", []) if s.get("name") == selected_server), None)
+        if server is None:
+            typer.echo(f"Server '{selected_server}' not found in the YAML file.")
+            raise typer.Exit(code=1)
+    return server
